a66cd04750450c4e556bb12ed7c8e1edf1c00426
Juan Carlos Guerrero <juanc.guerrero@s-dos.es>
2018-12-10 14:58:42 +0100

    Se añaden los mensajes de error y validaciones desde el backend y la vista de las mismas

--- src/app/components/clientes/form/form.component.ts
+++ src/app/components/clientes/form/form.component.ts
@@ -12,7 +12,8 @@ export class FormComponent implements OnInit {
 
   private cliente: Cliente = new Cliente();
   private titulo: string = "Crear Cliente"
-
+  private hayCambio: boolean = false;
+  private errores: String[];
 
   constructor(private _clienteService: ClienteService, private _router: Router, private _activatedRoute: ActivatedRoute) {
   }
@@ -25,13 +26,16 @@ export class FormComponent implements OnInit {
     this._clienteService.create(this.cliente).subscribe(
       response => {
         this._router.navigate(['/clientes']);
-        swal('Nuevo cliente', `Cliente ${response.nombre} creado con éxito`, 'success');
+        swal('Nuevo cliente', `El cliente ${response.nombre} ha sido creado con exito`, 'success');
       },
       error =>{
+        this.errores = error.error.errors as string[];
+        console.error("Código del error desde el backend: " + error.status);
+        console.log(error.error.errors);
         swal('Nuevo cliente', `Ha ocurrido un error al crear el cliente`, 'warning');
         console.log(error);
       }
-    )
+    );
   }
 
   public cargarCliente(): void{
@@ -51,13 +55,20 @@ export class FormComponent implements OnInit {
     this._clienteService.update(this.cliente).subscribe(
       response => {
         this._router.navigate(['/clientes']);
-        swal('Cliente Actualizado', `Cliente ${response.nombre} actualizado con éxito`, 'success');
+        swal('Cliente Actualizado', `${response.cliente.nombre}: ${response.cliente.mensaje}`, 'success');
       },
       error =>{
+        this.errores = error.error.errors as string[];
+        console.error("Código del error desde el backend: " + error.status);
+        console.log(error.error.errors);
         swal('Cliente Actualizado', `Ha ocurrido un error al crear el cliente`, 'warning');
         console.log(error);
       }
     )
+  }
+
+  onChange(event){
+    this.hayCambio = true;
   }
 
 }
--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,9 +1,11 @@
+import { Router } from '@angular/router';
 import { CLIENTES } from './../components/clientes/clientes.json';
 import { Injectable } from '@angular/core';
 import { Cliente } from '../model/cliente.js';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,7 @@ export class ClienteService {
   private urlEndPoint: string = 'http://localhost:8080/api/clientes'
   private httpHeaders = new HttpHeaders({'Content-Type':'application/json'})
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient, private _router: Router) { }
 
   getClientes(): Observable<Cliente[]> {
     return this._http.get<Cliente[]>(this.urlEndPoint); 
@@ -24,19 +26,55 @@ export class ClienteService {
   }
 
   create(cliente: Cliente) : Observable<Cliente> {
-    return this._http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders}); 
+    return this._http.post(this.urlEndPoint, cliente, {headers: this.httpHeaders}).pipe(
+      map((response: any) => response.cliente as Cliente),
+      catchError(error => {
+
+        if(error.status == 400){
+          return throwError(error);
+        }
+
+        console.log(error.error.mensaje);
+        swal(error.error.mensaje, error.error.error, 'error');
+        return throwError(error);
+      })
+    ); 
   }
 
   getCliente(id): Observable<Cliente>{
-    return this._http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+    return this._http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError(error => {
+        this._router.navigate(['/clientes']);
+        console.log(error.error.mensaje);
+        swal('Error al editar', error.error.mensaje, 'error');
+        return throwError(error);
+      })
+    );
   }
 
-  update(cliente: Cliente): Observable<Cliente>{
-    return this._http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders});
+  update(cliente: Cliente): Observable<any>{
+    return this._http.put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders}).pipe(
+      catchError(error => {
+
+        if(error.status == 400){
+          return throwError(error);
+        }
+
+        console.log(error.error.mensaje);
+        swal(error.error.mensaje, error.error.error, 'error');
+        return throwError(error);
+      })
+    );
   }
 
   delete(id: number): Observable<Cliente>{
-    return this._http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders});
+    return this._http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders}).pipe(
+      catchError(error => {
+        console.log(error.error.mensaje);
+        swal(error.error.mensaje, error.error.error, 'error');
+        return throwError(error);
+      })
+    );
   }
 
 }
